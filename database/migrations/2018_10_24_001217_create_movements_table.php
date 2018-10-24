2a709c2552068e5c7a9a76037af9381f0b3ad195
Daniel Jackson <djackson@uabc.edu.mx>
2018-10-23 17:19:57 -0700

    Modelos

---
+++ database/migrations/2018_10_24_001217_create_movements_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMovementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('type');
+            $table->dateTime('movement_date');
+            $table->unsignedInteger('categoru_id');
+            $table->string('desciption');
+            $table->unsignedInteger('money');
+            $table->string('image');
+            $table->unsignadInteger('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movements');
+    }
+}
