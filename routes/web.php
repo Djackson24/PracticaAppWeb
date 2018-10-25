e36876b64663bca4b0a067b3571f58dae3760e0c
Daniel Jackson <djackson@uabc.edu.mx>
2018-10-24 20:02:18 -0700

    com9

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,12 @@ Route::get('/movements/id/{id}',function($id){
 Route::get('/movements/desciption/{desciption}',function($desciption){
     $des = App\Movement::where('desciption',$desciption)->get();
     return $des;
-});
+})->where('desciption','[a-z]','[A-Z]');
 
-
+Route::get('/movements/movement_date/{year?}/{month?}/{day?}',function($movement_date){
+    $fecha = App\Movement::where('movement_date',$movement_date)->get();
+    return $fecha;
+})->where('movement_date','[year [0-9]+, month [0-9]+, day[0-9]+]');
 
 
 
